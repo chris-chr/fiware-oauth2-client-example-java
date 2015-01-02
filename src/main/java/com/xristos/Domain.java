b670a9d0540e2a580dcf85cca4e8c829c6b45de2
cchristodoulopoulos <christrip14@hotmail.com>
2015-01-02 14:54:16 +0200

    Update Domain.java

--- src/main/java/com/xristos/Domain.java
+++ src/main/java/com/xristos/Domain.java
@@ -4,7 +4,7 @@ public class Domain {
 	//application specific variables 
 	public static final String d = "localhost";
 	public static final String p = "8080"; //port
-	public static final String clientId = "1755";
-	public static final String clientSercet = "c7ce19d1f5dd6be111f613f951897a238d6ee10b86c35dbdc33f76cdd0ab635eea26d2e60134e82e7bf1f4504720d83d2dc46f46e4c7333b8b48cf7f28486967";
+	public static final String clientId = "";
+	public static final String clientSercet = "";
 	public static final String getTokenUrl = "https://account.lab.fi-ware.org/oauth2/token";
 }
